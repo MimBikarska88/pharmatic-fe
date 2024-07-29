2de967e3a7c1fa5de4b90b5d16c211808088f411
Mimi <mariabikarska2988@gmail.com>
2024-07-29 20:21:06 +0300

    Create home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,46 @@
 import { useUserStore } from "../../stores/userStore";
-
+import styles from "./Home.module.css";
+import second from "../../static/img/backgrounds/second.jpg";
+import first from "../../static/img/backgrounds/first.jpeg";
 const Home = (props) => {
   const Vendor = useUserStore((state) => state.Vendor);
-  console.log(Vendor.residence);
   return (
     <>
-      <h4>Home</h4>
+      <div className={styles["grid-container"]}>
+        <img className={styles.second} src={second} />
+        <div className={styles.third}>
+          <div className={styles.info}>
+            Join the Pharmatic Vendor Program and expand your business by
+            selling healthcare products on our trusted online platform.
+            Pharmatic offers a seamless and secure e-commerce solution designed
+            specifically for pharmaceutical vendors. As a vendor, you'll benefit
+            from our extensive customer base, user-friendly interface, and
+            robust marketing support.
+          </div>
+        </div>
+        <div className={styles.thirdHalf}>
+          <div className={styles.info}>
+            Manage your own business! If you are a vendor register{" "}
+            <a href="/register/vendor">here</a>
+          </div>
+        </div>
+        <img className={styles.first} src={first} />
+        <div className={styles.fourth}>
+          <div className={styles.info}>
+            Pharmatic is your trusted local pharmacy dedicated to providing
+            high-quality healthcare products and services. We offer a wide range
+            of prescription medications, over-the-counter drugs, vitamins, and
+            health supplements. Committed to your well-being, Pharmatic ensures
+            convenient and affordable access to all your health needs.
+          </div>
+        </div>
+        <div className={styles.fourthHalf}>
+          <div className={styles.info}>
+            If you would like to be our customer register{" "}
+            <a href="/register/customer">here</a>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
